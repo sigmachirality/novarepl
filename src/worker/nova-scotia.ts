edeb6309f7081bea6f8c28305e10980f46db8d6a
Daniel Tao <danieltaox@gmail.com>
2023-04-23 20:52:56 +0200

    feat: update nova-scotia worker interface

--- src/worker/nova-scotia.ts
+++ src/worker/nova-scotia.ts
@@ -2,28 +2,28 @@
 
 import { expose } from "comlink";
 
-async function generate_params() {
+async function generate_params(r1csUrl: string) {
   const multiThread = await import("nova_scotia_browser");
   await multiThread.default();
   await multiThread.initThreadPool(navigator.hardwareConcurrency);
 
-  return await multiThread.generate_params();
+  return await multiThread.generate_params(r1csUrl);
 }
 
-async function generate_proof(pp: string) {
+async function generate_proof(pp: string, inputs: any, r1csUrl: string, wasmUrl: string) {
   const multiThread = await import("nova_scotia_browser");
   await multiThread.default();
   await multiThread.initThreadPool(navigator.hardwareConcurrency);
 
-  return await multiThread.generate_proof(pp);
+  return await multiThread.generate_proof(pp, inputs, r1csUrl, wasmUrl);
 }
 
-async function verify_proof(pp: string, proof: string) {
+async function verify_proof(pp: string, inputs: any, proof: string) {
   const multiThread = await import("nova_scotia_browser");
   await multiThread.default();
   await multiThread.initThreadPool(navigator.hardwareConcurrency);
 
-  return await multiThread.verify_compressed_proof(pp, proof);
+  return await multiThread.verify_compressed_proof(pp, inputs, proof);
 }
 
 const exports = {
